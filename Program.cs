1360335b3cc482ec8ae3766ece9ff0e508a9e83e
Ilya <kumankov@gmail.ru>
2022-11-18 22:42:56 +0300

    11

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("введите a:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите b:");
+int b = int.Parse(Console.ReadLine());
+if (a>b)
+{
+Console.Write($"Max = a = {a}");
+Console.Write($" Min = b = {b}");
+}
+else
+if (b>a)
+{
+Console.Write($"Max = b = {b}");
+Console.Write($" Min = a = {a}");
+}
+else
+
+    Console.Write($"a = b= {a}");
+
